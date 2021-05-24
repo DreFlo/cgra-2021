5ef2d3f9316d64846c1ad6eb0fad86b301777847
Andre Flores <up201907001@fe.up.pt>
2021-05-24 14:17:46 +0100

    proj-8 state.

--- project/MyScene.js
+++ project/MyScene.js
@@ -45,7 +45,7 @@ export class MyScene extends CGFscene {
         this.incompleteSphere = new MySphere(this, 16, 8);
         this.movingObject = new MyMovingObject(this, Math.PI / 2, 0.0, 0, 0, 0);
         this.cilinder = new MyCilinder(this, 16);
-        this.fish = new MyFish(this, 0.2, "images/scale.png", [0.76, 0.54, 0.89], [0, 3, 0]);
+        this.fish = new MyMovingFish(this, 0.2, 0.0, [0, 5, 0], [0.76, 0.54, 0.89], "images/scale.png");
         this.seaFloor = new MySeaFloor(this, 20, 50, 1);
         this.seaSurface = new MyWaterSurface(this);
         this.rocks = new MyRockSet(this);
@@ -237,40 +237,40 @@ export class MyScene extends CGFscene {
         // Check for key codes e.g. in https://keycode.info/
         if (this.gui.isKeyPressed("KeyW")) {
             this.movingObject.accelerate(-0.1);
-            //this.fish.accelerate(-0.1);
+            this.fish.accelerate(-0.1);
             text += " W ";
             keysPressed = true;
         }
 
         if (this.gui.isKeyPressed("KeyS")) {
             this.movingObject.accelerate(-0.1);
-            //this.fish.accelerate(0.1);
+            this.fish.accelerate(0.1);
             text += " S ";
             keysPressed = true;
         }
 
         if (this.gui.isKeyPressed("KeyA")) {
             this.movingObject.turn(0.1);
-            //this.fish.turn(0.1);
+            this.fish.turn(0.1);
             text += " A ";
             keysPressed = true;
         } else if (this.gui.isKeyPressed("KeyD")) {
             this.movingObject.turn(-0.1);
-            //this.fish.turn(-0.1);
+            this.fish.turn(-0.1);
             text += " D ";
             keysPressed = true;
         } else {
             this.movingObject.turn(0);
-            //this.fish.turn(0);
+            this.fish.turn(0);
         }
 
         if (this.gui.isKeyPressed("KeyR")) {
             this.movingObject.reset();
-            //this.fish.reset();
+            this.fish.reset();
             text += " R ";
             keysPressed = true;
         }
-    /*
+
         if (this.gui.isKeyPressed("KeyP")) {
             this.fish.elevate();
             text += " P ";
@@ -290,8 +290,6 @@ export class MyScene extends CGFscene {
             text += " C ";
             keysPressed = true;
         }
-
-     */
 
         if (keysPressed) {
             console.log(text);
